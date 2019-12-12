0009ae34de54b65a7d98c820d40b7544396e1d8e
Random Cosmos <srijanwhat2@gmail.com>
2019-12-12 15:32:28 +0530

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,26 +1,26 @@
 $(function(){
     $(".f").on("mouseover",function(){
-        $(this).attr("src", "../images/facebook.svg");
+        $(this).attr("src", "images/facebook.svg");
     });
     $(".f").on("mouseout",function(){
-        $(this).attr("src", "../images/facebook-before.svg");
+        $(this).attr("src", "images/facebook-before.svg");
     })
 });
 
 $(function(){
     $(".i").on("mouseover",function(){
-        $(this).attr("src", "../images/instagram.svg");
+        $(this).attr("src", "images/instagram.svg");
     });
     $(".i").on("mouseout",function(){
-        $(this).attr("src", "../images/instagram-before.svg");
+        $(this).attr("src", "images/instagram-before.svg");
     })
 });
 
 $(function(){
     $(".t").on("mouseover",function(){
-        $(this).attr("src", "../images/twitter.svg");
+        $(this).attr("src", "images/twitter.svg");
     });
     $(".t").on("mouseout",function(){
-        $(this).attr("src", "../images/twitter-before.svg");
+        $(this).attr("src", "images/twitter-before.svg");
     })
 });
